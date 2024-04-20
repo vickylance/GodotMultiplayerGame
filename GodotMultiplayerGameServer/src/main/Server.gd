40ba8de6ded4a77665a691195ef99f52363aa388
Vignesh S <vignesh6993@live.in>
2024-04-20 14:52:12 +0530

    Initial connection established between client and server

---
+++ GodotMultiplayerGameServer/src/main/Server.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var network := ENetMultiplayerPeer.new()
+var port := 1909
+var max_players := 100
+
+func _ready() -> void:
+    start_server()
+
+func start_server() -> void:
+    network.create_server(port, max_players)
+    multiplayer.multiplayer_peer = network
+    print("Server started on port: " + str(port))
+
+    network.peer_connected.connect(_peer_connected)
+    network.peer_disconnected.connect(_peer_disconnected)
+    pass
+
+func _peer_connected(player_id) -> void:
+    print("Player connected: " + str(player_id))
+    pass
+
+func _peer_disconnected(player_id) -> void:
+    print("Player disconnected: " + str(player_id))
+    pass

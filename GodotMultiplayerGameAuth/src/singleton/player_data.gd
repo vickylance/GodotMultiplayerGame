4329ab01be252cbdf812fd9ce6dd000d5a6eee62
Vignesh S <vignesh6993@live.in>
2026-01-31 01:12:33 +0530

    feat(project-upgrade): Upgraded GodotMultiplayerGameAuth to Godot 4.6 mono

--- GodotMultiplayerGameAuth/src/singleton/player_data.gd
+++ GodotMultiplayerGameAuth/src/singleton/player_data.gd
@@ -1,25 +1,33 @@
 extends Node
 
 var player_data: Dictionary = {}
-
+const DATA_FOLDER = "user://data/"
+const PLAYER_DATA = "player_data.json"
 
 func _ready() -> void:
 	print(OS.get_user_data_dir())
-	if not DirAccess.dir_exists_absolute("user://data"):
-		DirAccess.make_dir_recursive_absolute("user://data")
+	if not DirAccess.dir_exists_absolute(DATA_FOLDER):
+		DirAccess.make_dir_recursive_absolute(DATA_FOLDER)
 	load_player_ids()
 	pass
 
 
 func load_player_ids() -> void:
-	var player_data_file := FileAccess.open(("user://data/player_data.json"), FileAccess.READ)
+	# Check if file exists if not create
+	if not FileAccess.file_exists(DATA_FOLDER + PLAYER_DATA):
+		save_player_ids()
+	
+	var player_data_file := FileAccess.open(DATA_FOLDER + PLAYER_DATA, FileAccess.READ)
+	if player_data_file == null:
+		var error_str = error_string(FileAccess.get_open_error())
+		push_error("Couldn't open file because: %s" % error_str)
 	player_data = JSON.parse_string(player_data_file.get_as_text())
 	player_data_file.close()
 	pass
 
 
 func save_player_ids() -> void:
-	var player_data_file := FileAccess.open(("user://data/player_data.json"), FileAccess.WRITE)
+	var player_data_file := FileAccess.open(DATA_FOLDER + PLAYER_DATA, FileAccess.WRITE)
 	player_data_file.store_line(JSON.stringify(player_data))
 	player_data_file.close()
 	pass
